7bd51ba0563e01fa33742ef9fd5b556eed08e7b0
Olalekan Yusuf <olasyusuf@gmail.com>
2025-11-23 00:07:16 +0100

    feat: working with array

--- ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Console.WriteLine("Hello, My first C# Program!");
-class Program
-{
-    // static void Main(string[] args)
-    // {
-    //     Console.WriteLine("Hello, My first C# Program!");
-    // }
-}
