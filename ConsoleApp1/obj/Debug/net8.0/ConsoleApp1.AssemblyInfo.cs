f131c58af76c15fc713ae3975e10d774c0ea1149
Olalekan Yusuf <olasyusuf@gmail.com>
2025-11-23 00:11:14 +0100

    feat: collections and generic - concepts

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0124123cfd19e578e7f9298f6749b0a3a64c5932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28705b1c30b19d0e182d058568d185b68261a2c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
