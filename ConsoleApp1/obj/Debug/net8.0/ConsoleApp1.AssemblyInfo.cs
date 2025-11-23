f72f57a536e06e4f667a57f8b0873a37fc07cf2e
Olalekan Yusuf <olasyusuf@gmail.com>
2025-11-23 01:57:23 +0100

    feat: oop - concepts

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f131c58af76c15fc713ae3975e10d774c0ea1149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394a61057c507e29a987425183d4cde4f8fb5e12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
