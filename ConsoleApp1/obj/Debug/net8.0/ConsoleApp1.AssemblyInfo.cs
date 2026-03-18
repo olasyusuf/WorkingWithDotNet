874efda54f52d209212dff5c36bea9efdcbb6588
Olalekan Yusuf <olasyusuf@gmail.com>
2026-03-18 16:24:24 +0100

    feat: practicals - finalize PurchaseOrder Event using Delegate

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d79a2d33abb6327bdba35c1deb1fb5dd48881f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3e9a978690da46783a8b593d4c87d8f35c245c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
